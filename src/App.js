8a1473813cceb94563eb9a32ffb4b71852145efc
Vallyblink <115736687+Vallyblink@users.noreply.github.com>
2023-11-30 09:54:57 +0200

    work?

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit App and save to reload.WORK YEAH
         </p>
         <a
           className="App-link"
